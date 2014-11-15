a28cdaea7949a4ba2a12f70aff2bb87ca8a43b48
zkropotkine <s.daniel.rodriguez@gmail.com>
2014-11-15 02:50:52 -0600

    Default

--- app/src/main/java/com/example/zkropotkine/myapplication/AndroidFirstApp.java
+++ app/src/main/java/com/example/zkropotkine/myapplication/AndroidFirstApp.java
@@ -15,8 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AndroidFirstApp extends Activity implements TabListener {
-
-    List<Fragment> tabFragmentList = new ArrayList<Fragment>();
+    private String[] tabNames = {"Name", "Last Name", "Age","Image",  "Main"};
+    private String name = null;
+    private String lastName = null;
+    private String age = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,14 @@ public class AndroidFirstApp extends Activity implements TabListener {
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        for (int i=1; i <= 4; i++) {
+        for (int i = 1; i <= 5; i++) {
             Tab tab = actionBar.newTab();
-            tab.setText("Tab " + i);
+            tab.setText(tabNames[i - 1]);
             tab.setTabListener(this);
             actionBar.addTab(tab);
         }
+
+        actionBar.setSelectedNavigationItem(4);
     }
 
     @Override
@@ -38,51 +42,56 @@ public class AndroidFirstApp extends Activity implements TabListener {
         Fragment fragment = null;
         TabFragment tabFragment = null;
 
-        if (tabFragmentList.size() > tab.getPosition()) {
-            fragment = tabFragmentList.get(tab.getPosition());
+        tabFragment = new TabFragment();
+        Bundle bundle = new Bundle();
+        int colorResId = 0;
+
+        if (tab.getPosition() == 0) {
+            colorResId = R.color.color1;
+            lastName = null;
+            age = null;
+        } else if (tab.getPosition() == 1) {
+            name = null;
+            age = null;
+            //imgResId = getResources().getIdentifier("parrot", "drawable", "com.javapapers.android.androidtablayout.app");
+            colorResId = R.color.color2;
+        } else if (tab.getPosition() == 2) {
+            name = null;
+            lastName = null;
+            //imgResId = getResources().getIdentifier("cock", "drawable", "com.javapapers.android.androidtablayout.app");
+            colorResId = R.color.color3;
+        } else if (tab.getPosition() == 3) {
+            name = null;
+            lastName = null;
+            age = null;
+            //imgResId = getResources().getIdentifier("cock", "drawable", "com.javapapers.android.androidtablayout.app");
+            colorResId = R.color.color1;
         }
 
-        if (fragment == null) {
-            tabFragment = new TabFragment();
-            Bundle bundle = new Bundle();
-            int imgResId = 0;
-            int colorResId = 0;
-            if(tab.getPosition()==0){
-                TextView myText = (TextView) findViewById(R.id.textView);
+        bundle.putString("name", name);
+        bundle.putString("lastName", lastName);
+        bundle.putString("age", age);
+        bundle.putInt("color", colorResId);
+        bundle.putInt("tabNum", tab.getPosition());
 
-                Animation anim = new AlphaAnimation(0.0f, 1.0f);
-                anim.setDuration(50); //You can manage the time of the blink with this parameter
-                anim.setStartOffset(20);
-                anim.setRepeatMode(Animation.REVERSE);
-                anim.setRepeatCount(Animation.INFINITE);
-                myText.startAnimation(anim);
-                //imgResId = getResources().getIdentifier("duck", "drawable","com.javapapers.android.androidtablayout.app");
-                colorResId = R.color.color1;
-            } else if (tab.getPosition()==1){
-                //imgResId = getResources().getIdentifier("parrot", "drawable", "com.javapapers.android.androidtablayout.app");
-                colorResId = R.color.color2;
-            } else if(tab.getPosition()==2){
-                //imgResId = getResources().getIdentifier("cock", "drawable", "com.javapapers.android.androidtablayout.app");
-                colorResId = R.color.color3;
-            }
-            //bundle.putInt("image", imgResId);
-            bundle.putInt("color", colorResId);
-            tabFragment.setArguments(bundle);
-            tabFragmentList.add(tabFragment);
-        }
-        else {
-            tabFragment = (TabFragment) fragment;
-        }
+        tabFragment.setArguments(bundle);
+
         ft.replace(android.R.id.content, tabFragment);
     }
 
     @Override
     public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        if (tabFragmentList.size() > tab.getPosition()) {
-            ft.remove(tabFragmentList.get(tab.getPosition()));
-        }
+        TextView nameText = (TextView) findViewById(R.id.editText);
+        TextView lastNameText = (TextView) findViewById(R.id.editText2);
+        TextView ageText = (TextView) findViewById(R.id.editText3);
+
+
+        name = nameText.getText().toString();
+        lastName = lastNameText.getText().toString();
+        age = ageText.getText().toString();
     }
 
     @Override
-    public void onTabReselected(Tab tab, FragmentTransaction ft) {  }
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
 }
--- app/src/main/java/com/example/zkropotkine/myapplication/TabFragment.java
+++ app/src/main/java/com/example/zkropotkine/myapplication/TabFragment.java
@@ -1,30 +1,103 @@
 package com.example.zkropotkine.myapplication;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Property;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 public class TabFragment extends Fragment {
 
-    private int image;
+    private String name;
+    private String lastName;
+    private String age;
     private int color;
+    private int tabNum;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getArguments();
-        image = bundle.getInt("image");
+        name = bundle.getString("name");
+        lastName = bundle.getString("lastName");
+        age = bundle.getString("age");
         color = bundle.getInt("color");
+        tabNum = bundle.getInt("tabNum");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tab, null);
-        ImageView tabImage = (ImageView) view.findViewById(R.id.imageView);
-        tabImage.setImageResource(image);
+        //ImageView tabImage = (ImageView) view.findViewById(R.id.imageView);
+
+
+        TextView myText = (TextView) view.findViewById(R.id.textView);
+
+        String[] texts = {name, lastName, age};
+
+
+        for (String text : texts) {
+            if (text != null && tabNum != 3) {
+                myText.setText(text);
+
+                if (tabNum == 1) {
+                    Animation anim = new AlphaAnimation(0.0f, 1.0f);
+                    anim.setDuration(1000); //You can manage the time of the blink with this parameter
+                    //anim.setStartOffset(20);
+                    anim.setBackgroundColor(Color.BLUE);
+                    anim.setRepeatMode(Animation.REVERSE);
+                    anim.setRepeatCount(Animation.INFINITE);
+
+                    myText.startAnimation(anim);
+                } else if (tabNum == 2) {
+
+                    final Property<TextView, Integer> property = new Property<TextView, Integer>(int.class, "textColor") {
+                        @Override
+                        public Integer get(TextView object) {
+                            return object.getCurrentTextColor();
+                        }
+
+                        @Override
+                        public void set(TextView object, Integer value) {
+                            object.setTextColor(value);
+                        }
+                    };
+
+                    myText.setTextColor(Color.BLUE);
+
+                    final ObjectAnimator animator = ObjectAnimator.ofInt(myText, property, Color.RED);
+                    animator.setDuration(666L);
+                    animator.setEvaluator(new ArgbEvaluator());
+                    animator.setInterpolator(new DecelerateInterpolator(2));
+                    animator.setRepeatCount(ObjectAnimator.INFINITE);
+                    animator.setRepeatMode(ObjectAnimator.REVERSE);
+                    animator.start();
+
+                }  else if(tabNum == 4) {
+                    myText.setText("");
+                }
+
+                break;
+            }
+            else if (tabNum == 3) {
+                ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+                imageView.setImageResource(R.drawable.ic_launcher);
+                myText.setText("");
+                break;
+            }
+        }
+
         view.setBackgroundResource(color);
         return view;
     }
